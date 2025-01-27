85db86177259ddcdc1e5d01526df01333f83d543
MOHAMED-RGUIG <135667611+MOHAMED-RGUIG@users.noreply.github.com>
2025-01-27 15:23:50 +0100

    Update CheckoutValidinv.js

--- src/components/CheckoutValidinv.js
+++ src/components/CheckoutValidinv.js
@@ -16,7 +16,7 @@ function CheckoutValidinv({ REFINV_0, ITMREF_0, localData, resetInputs }) {
     const dispatch = useDispatch();
 
     function tokenHandler() {
-        if (REFINV_0 && ITMREF_0 ) {
+
             const payload = {
                 REFINV_0,
                 ITMREF_0,
@@ -29,14 +29,6 @@ function CheckoutValidinv({ REFINV_0, ITMREF_0, localData, resetInputs }) {
                 hideProgressBar: false
             });
             resetInputs();
-        } else {
-            toast.error('Oops ! Merci de remplir le formulaire et d’ajouter des lignes !', {
-                position: 'bottom-right',
-                autoClose: 3000,
-                hideProgressBar: false
-            });
-            
-        }
     }
 
     return (
