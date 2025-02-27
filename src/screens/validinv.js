7c7935aaa1051ff2e8834e5646ff21fe2460fe4e
mohamed <mohamedrguig26@gmail.com>
2025-02-27 16:00:26 +0100

    Description de la modification

--- src/screens/validinv.js
+++ src/screens/validinv.js
@@ -253,10 +253,10 @@ const handleAddQt = () => {
         setLocalData([]);
     };
     useEffect(() => {
-      if (REFINV_0, ITMREF_0) {
+      if (REFINV_0 && ITMREF_0) {
         handleInvSelection(REFINV_0, ITMREF_0);
       }
-    }, [REFINV_0]);
+    }, [REFINV_0,ITMREF_0]);
      useEffect(() => {
     if (EANCOD_0.trim() !== "") {
       const matchedItem = localData.find((item) => item.EANCOD_0 === EANCOD_0);
